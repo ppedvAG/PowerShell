dcdfe98f20c04f65ff55ecf91d2ee4993f57f71c
Eik Klee <34546612+EikKlee@users.noreply.github.com>
2019-04-01 12:58:35 +0200

    Add files via upload

---
+++ Aus den Folien/Begin-Process-End.ps1
@@ -0,0 +1,25 @@
+﻿	Function Test-Demo
+{
+	[cmdletbinding()]
+
+  Param (
+  	[parameter(ValueFromPipeline=$True)]
+	[string[]]$InputObj,
+  
+	[String]$Param1
+  )
+  Begin
+  {
+    Write-Host "Start"
+  }
+  Process
+  { 
+    echo "Verarbeite $InputObj mit $Param1" #Statt $InputObj kann auch $_ verwendet werden
+  }
+  End
+  {
+    echo "Ende"
+  }
+}
+
+"Objekt1", "Objekt2" | Test-Demo -Param1 Parameter
